65f3507183912cb529117a6c2ab3ca431a9501d6
oskarrahlen <rahlenoskar@gmail.com>
2020-03-26 11:08:17 +0100

    Fixed subpages

--- src/Pages/Page/Page.tsx
+++ src/Pages/Page/Page.tsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 import Flexible from "../../Components/Global/Flexible/Flexible";
 import { GetPageBySlug } from "../../Services/Pages/Pages";
 import Error404Template from "../../PageTemplates/Error404Template/Error404Template";
 
-const Page: React.FC = () => {
-  const { slug } = useParams();
+const Page: React.FC = (props) => {
+  const params = useParams();
+  const {slug} = useParams();
+  let location = useLocation();
 
+  console.log(slug, "slug")
   const [loading, setLoading] = useState<boolean>(true);
   const [data, setData] = useState<any>();
   const [is404, set404] = useState<boolean>();
@@ -15,7 +18,7 @@ const Page: React.FC = () => {
     if (!slug) {
       return;
     }
-    GetPageBySlug(slug).then(resp => {
+    GetPageBySlug(location.pathname).then(resp => {
       if (Array.isArray(resp)) {
         set404(true);
       }
