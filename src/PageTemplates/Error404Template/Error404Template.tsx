b0acea043486724200ea94521e198e419609d4fe
Joakim Carlsten <joakim@vinnia.se>
2020-05-26 12:43:47 +0200

    Added explanatory text on 404 page

--- src/PageTemplates/Error404Template/Error404Template.tsx
+++ src/PageTemplates/Error404Template/Error404Template.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
+//TODO 2020-05-26: this url should be configurable
 const Error404Template : React.FC = () => {
     return (
         <div className="error-page">
             <div className="main">
                 <div className="main-inner">
                     <h1>404</h1>
+                    <p style={{textAlign: "center"}}>We're sorry, but something went wrong. Please start over again at <a href="https://www.ysds.com">www.ysds.com</a></p>
                 </div>
             </div>
         </div>
