b982e8835b978555566561defececd4c7007e5bb
oskarrahlen <rahlenoskar@gmail.com>
2020-06-09 08:43:32 +0200

    Fixed external links from linkbutton

--- src/Components/Global/LinkButton/LinkButton.tsx
+++ src/Components/Global/LinkButton/LinkButton.tsx
@@ -9,7 +9,13 @@ const LinkButton : React.FC<WPButton> = ({ button, button_style }) => {
 
   const url = new URL(button.url);
   const { pathname } = url;
-
+  if(!url.href.startsWith(window.location.host)){
+    return (
+      <a href={url.href} className={`ysds-button ${button_style}`}> 
+              {button.title}
+      </a>
+    )
+  }
   return (
     <Link to={pathname} className={`ysds-button ${button_style}`}>
       {button.title}
