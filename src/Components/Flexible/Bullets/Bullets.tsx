20ca96ea975f9e0179bdc8ba3f028e34691e8b55
Esther Alenius <>
2020-06-15 11:08:11 +0200

    updated bullets

--- src/Components/Flexible/Bullets/Bullets.tsx
+++ src/Components/Flexible/Bullets/Bullets.tsx
@@ -6,21 +6,23 @@ export type BulletProps = {
   bullets: [{ title: string, text: string, icon: string, fontawsome_or_upload: string, image: WPImage }],
   style: string,
   title: string,
-  columns:string
+  columns:string,
+  background_color:string,
+  bullet_alignment: string
 
   acf_fc_layout: 'check_list'
 
 };
 
-const Bullets: React.FC<BulletProps> = ({ bullets, style, title, columns}) => (
-  <section className="check-list">
+const Bullets: React.FC<BulletProps> = ({ bullets, style, title, columns, background_color, bullet_alignment}) => (
+  <section className={"check-list " + background_color}>
     <div className="main">
       <div className="main-inner">
         <h2>{title}</h2>
         <div className={`flex-container ${style} ${columns}`}>
           {
             bullets.map((bullet, i) => (
-              <div className="bullet" key={i}>
+              <div className={`bullet ${bullet_alignment}`} key={i}>
                 {
                   bullet.fontawsome_or_upload === 'upload' ? (<img alt="" style={{height:62}} src={bullet.image.url} />) : <div className="check-mark" dangerouslySetInnerHTML={{ __html: bullet.icon }} />
                 }
