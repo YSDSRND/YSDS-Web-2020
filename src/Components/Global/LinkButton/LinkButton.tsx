f2d5a58fe451bfc2ae19e8ce39c6b7f717e7c24c
oskarrahlen <rahlenoskar@gmail.com>
2020-06-21 11:56:23 +0200

    Implemented target

--- src/Components/Global/LinkButton/LinkButton.tsx
+++ src/Components/Global/LinkButton/LinkButton.tsx
@@ -2,22 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import WPButton from '../../../Util/Types/WPButton';
 
-const LinkButton : React.FC<WPButton> = ({ button, button_style }) => {
+const LinkButton : React.FC<WPButton> = ({ button, button_style,  }) => {
   if (!button) {
     return <div />;
   }
 
   const url = new URL(button.url);
   const { pathname } = url;
+
+  
   if(!url.href.startsWith(window.location.host)){
     return (
-      <a href={url.href} className={`ysds-button ${button_style}`}> 
+      <a target={button.target} href={url.href} className={`ysds-button ${button_style}`}> 
               {button.title}
       </a>
     )
   }
   return (
-    <Link to={pathname} className={`ysds-button ${button_style}`}>
+    <Link to={pathname} target={button.target} className={`ysds-button ${button_style}`}>
       {button.title}
     </Link>
   );
