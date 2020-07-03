4e20e341f108c42fdd0039cccc600cc5543701ae
Joakim Carlsten <joakim@vinnia.se>
2020-07-03 17:06:49 +0200

    Again, adding date to the posts cards

--- src/Components/Global/PostCard/PostCard.tsx
+++ src/Components/Global/PostCard/PostCard.tsx
@@ -5,7 +5,7 @@ const PostCard : React.FC<any> = ({ post }) => (
     <img alt="" src={post.media ? post.media.large : ''} />
     <div className="post-text-container">
       <h3>{post.title}</h3>
-      <p className="date">27 maj 2020</p>
+      <p className="date">{new Date(post.date).toLocaleDateString('sv-SE')}</p>
       <p>{post.excerpt}</p>
     </div>
   </a>
