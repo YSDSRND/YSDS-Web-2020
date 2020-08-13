dea17113a61450d7017ef183dfe843bc5e9c9f5a
Noah Olsson <noaholsson@hotmail.com>
2020-08-13 10:04:00 +0200

    Added privacy policy to contact form

--- src/Components/Global/ContactForm/ContactForm.tsx
+++ src/Components/Global/ContactForm/ContactForm.tsx
@@ -1,70 +1,98 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {CountryDropdown} from "../CountryDropdown/CountryDropdown";
+import {Link} from "react-router-dom";
 
 const ContactForm: React.FC = () => {
-  const [name, setName] = useState('');
-  const [phone, setPhone] = useState('');
-  const [email, setEmail] = useState('');
-  const [company, setCompany] = useState('');
-  const [country, setCountry] = useState('');
-  const [send, setSend] = useState(false);
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [email, setEmail] = useState('');
+    const [company, setCompany] = useState('');
+    const [country, setCountry] = useState('');
+    const [send, setSend] = useState(false);
+    const [privacyPolicy, setPrivacyPolicy] = useState(false);
+    const [error, setError] = useState(false);
 
-  const onSubmit = (event: any) => {
-    event.preventDefault();
-    const type = "contact_us";
-    fetch('https://wp-admin.ysds.com/wp-json/qte/v1/contact', {
-      method: 'POST',
-      body: JSON.stringify({
-        name,
-        phone,
-        email,
-        company,
-        type,
-        country,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }).then((res: any) => {
-      setSend(true);
-    });
-  };
+    const onSubmit = (event: any) => {
+        event.preventDefault();
+        setError(false);
 
-  const setNameFunc = (e:any) => { setName(e.target.value); };
-  const setCompanyFunc = (e:any) => { setCompany(e.target.value); };
-  const setPhoneFunc = (e:any) => { setPhone(e.target.value); };
-  const setEmailFunc = (e:any) => { setEmail(e.target.value); };
+        if (!privacyPolicy) {
+            setError(true);
+            return;
+        }
+
+        const type = "contact_us";
+        fetch('https://wp-admin.ysds.com/wp-json/qte/v1/contact', {
+            method: 'POST',
+            body: JSON.stringify({
+                name,
+                phone,
+                email,
+                company,
+                type,
+                country,
+                privacy_policy: privacyPolicy,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then((res: any) => {
+            setSend(true);
+        });
+    };
+
+    const setNameFunc = (e: any) => {
+        setName(e.target.value);
+    };
+    const setCompanyFunc = (e: any) => {
+        setCompany(e.target.value);
+    };
+    const setPhoneFunc = (e: any) => {
+        setPhone(e.target.value);
+    };
+    const setEmailFunc = (e: any) => {
+        setEmail(e.target.value);
+    };
+    const setPrivacyPolicyFunc = (e: any) =>{
+        setPrivacyPolicy(e.target.checked);
+    }
 
 
-  return (
-    <div>
-      {
+    return (
+        <div>
+            {
                 send ? (
-                  <h3>Thanks! We will get back to you as soon as possible.</h3>
+                    <h3>Thanks! We will get back to you as soon as possible.</h3>
                 ) : (
-                  <form onSubmit={onSubmit}>
-                    <label>Name</label>
-                    <input type="text" name="name" value={name} onChange={setNameFunc} />
+                    <form onSubmit={onSubmit}>
+                        <label>Name</label>
+                        <input type="text" name="name" value={name} onChange={setNameFunc}/>
 
-                    <label>Company</label>
-                    <input type="text" name="company" value={company} onChange={setCompanyFunc} />
+                        <label>Company</label>
+                        <input type="text" name="company" value={company} onChange={setCompanyFunc}/>
 
-                    <label>Phone nr</label>
-                    <input type="tel" name="phone" value={phone} onChange={setPhoneFunc} />
+                        <label>Phone nr</label>
+                        <input type="tel" name="phone" value={phone} onChange={setPhoneFunc}/>
 
-                    <label>Email</label>
-                    <input type="email" name="email" value={email} onChange={setEmailFunc} />
+                        <label>Email</label>
+                        <input type="email" name="email" value={email} onChange={setEmailFunc}/>
 
-                    <label>Country</label>
-                    <CountryDropdown value={country} onChange={country => setCountry(country)} />
+                        <label>Country</label>
+                        <CountryDropdown value={country} onChange={country => setCountry(country)}/>
 
-                    <input type="submit" value="SEND" className="ysds-button" />
-                  </form>
+                        <label>
+                            <input type="checkbox" name="privacy_policy" checked={privacyPolicy} onChange={setPrivacyPolicyFunc} />
+                            I argee to the following <Link to="/privacy-policy" target="_blank">Privacy policy</Link> *
+                        </label>
+                        { error ? <p className="error">You need to agree to the privacy policy.</p> : null}
+
+                        <input type="submit" value="SEND" className="ysds-button"/>
+                    </form>
                 )
             }
-    </div>
+        </div>
 
-  );
+    );
 };
 
 export default ContactForm;
