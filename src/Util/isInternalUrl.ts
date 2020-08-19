b1659ab93500560b19abcc07a2c2480705cddeca
Noah Olsson <noaholsson@hotmail.com>
2020-08-19 10:24:23 +0200

    Fixed a bug in isInternalUrl when in production

--- src/Util/isInternalUrl.ts
+++ src/Util/isInternalUrl.ts
@@ -1,4 +1,4 @@
 export const isInternalUrl = (link: string): boolean => {
     const url = new URL(link);
-    return process.env.NODE_ENV !== 'production' ? url.href.startsWith('https://ysds.netlify.app') : url.href.startsWith(window.location.host)
+    return process.env.NODE_ENV !== 'production' ? url.href.startsWith('https://ysds.netlify.app') : url.href.startsWith(window.location.origin)
 }
