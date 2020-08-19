c42cbb9725c2591d9672063a6ae9431ec308cf32
Noah Olsson <noaholsson@hotmail.com>
2020-08-19 11:33:25 +0200

    Fixed a bug causing isInternalUrl to throw exception when an invalid URL was supplied.

--- src/Util/isInternalUrl.ts
+++ src/Util/isInternalUrl.ts
@@ -1,4 +1,9 @@
 export const isInternalUrl = (link: string): boolean => {
-    const url = new URL(link);
-    return process.env.NODE_ENV !== 'production' ? url.href.startsWith('https://ysds.netlify.app') : url.href.startsWith(window.location.origin)
+    try {
+        const url = new URL(link);
+        return process.env.NODE_ENV !== 'production' ? url.href.startsWith('https://ysds.com') : url.href.startsWith(window.location.origin)
+    } catch (e) {
+        return false;
+    }
+
 }
