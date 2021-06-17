eb3a25aab236bf88ec1c5154cc0364b569ec2e7f
Joakim Carlsten <joakim@vinnia.se>
2021-06-17 13:24:02 +0200

    Added colors and support for background image in bullets

--- src/Components/Flexible/Hero/Hero.tsx
+++ src/Components/Flexible/Hero/Hero.tsx
@@ -34,7 +34,6 @@ const Hero: React.FC<HeroProps> = ({
         <div className="background-image"
              style={{backgroundImage: `url(${backgroundImage && backgroundImage.sizes && backgroundImage.sizes.large ? backgroundImage.sizes.large : ''})`}}/>
         <div className="main">
-            <div className="triangle"/>
             <div className="main-inner">
                 <div className="text-container">
                     {
