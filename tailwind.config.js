72f5ec35c132a6208c5e1dc1310f65ab223b21ab
Joakim Carlsten <joakim@vinnia.se>
2021-07-02 14:52:25 +0200

    Added purge information to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.ts',
+    './src/**/*.tsx',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
